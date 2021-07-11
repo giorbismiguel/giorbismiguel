0f0e8b56f4c880cc7ae99938d01f9e23c4bd0b78
Giorbis Miguel <giorbismiguel@gmail.com>
2021-07-11 00:18:16 -0400

    Change testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,7 +8,7 @@ export default function Testimonials() {
       <div className="container px-5 py-10 mx-auto text-center">
         <UsersIcon className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
-          Client Testimonials
+          Programmer Testimonials
         </h1>
         <div className="flex flex-wrap m-4">
           {testimonials.map((testimonial) => (
