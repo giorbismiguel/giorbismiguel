fc7ee4fbc5547a0a5b0434a03499a263f2a69437
Giorbis Miguel <giorbismiguel@gmail.com>
2021-08-18 21:55:19 -0400

    Add new React Native project

--- src/data.js
+++ src/data.js
@@ -24,27 +24,28 @@ export const projects = [
     link: "https://reactbootcamp.com",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    title: "Galipor",
+    subtitle: "React, React Native Expo",
+    description: "Development a software to control Swine feeding",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "http://galiportdb.geg.cu/graphql",
   },
 ];
 
 export const testimonials = [
   {
-    quote:`Giorbis is a excelent team leader, with the ability to help through constructive criticism.
+    quote: `Giorbis is a excelent team leader, with the ability to help through constructive criticism.
     Their experience and willingness to provide help, they allow to integrate into the work team in an easier way. He is a person who advocates and encourages professional and personal improvement, constantly,
     showing that there are no limits to continue learning.`,
-    image: "https://media-exp1.licdn.com/dms/image/C4D03AQGHN8X7IHXULQ/profile-displayphoto-shrink_200_200/0/1558702589286?e=1631145600&v=beta&t=JWf5grf2uiXIynIkGLiVrb-axN6A6MUjoEZ6orkhRKc",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQGHN8X7IHXULQ/profile-displayphoto-shrink_200_200/0/1558702589286?e=1631145600&v=beta&t=JWf5grf2uiXIynIkGLiVrb-axN6A6MUjoEZ6orkhRKc",
     name: "Ronny Garcia Lorenzo",
     company: "Smart145",
   },
   {
-    quote:`Giorbis is a great mind. Think of things the rest of us overlook. As a result, he has led his team to discover new ideas and to sow the spirit of creativity in each one of us. He quickly assumes new responsibilities while having time to teach and share his knowledge with the other team members.`,
-    image: "https://media-exp1.licdn.com/dms/image/C4D03AQE86R1gnKOm9w/profile-displayphoto-shrink_200_200/0/1552401381051?e=1631145600&v=beta&t=n203NfDmnPgTCZyUBoM7M95orYpiIY5N-9I8wuA_9XE",
+    quote: `Giorbis is a great mind. Think of things the rest of us overlook. As a result, he has led his team to discover new ideas and to sow the spirit of creativity in each one of us. He quickly assumes new responsibilities while having time to teach and share his knowledge with the other team members.`,
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQE86R1gnKOm9w/profile-displayphoto-shrink_200_200/0/1552401381051?e=1631145600&v=beta&t=n203NfDmnPgTCZyUBoM7M95orYpiIY5N-9I8wuA_9XE",
     name: "José Carlos",
     company: "Smart145",
   },
